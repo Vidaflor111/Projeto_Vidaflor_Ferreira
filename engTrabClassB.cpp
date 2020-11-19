2522b6ab007016b613df1a884921fdf0c6fb4ee7
Vidaflor111 <74689505+Vidaflor111@users.noreply.github.com>
2020-11-19 17:12:13 -0300

    Criação do metodo MC3()

--- engTrabClassB.cpp
+++ engTrabClassB.cpp
@@ -9,6 +9,7 @@ class B{
 	public:
 		void MB1();
 		void MB2();
+		void MB3();
 };
 void B::MB1(){
 	cout<<"MB1"<<endl;
@@ -16,10 +17,13 @@ void B::MB1(){
 void B::MB2(){
 	cout<<"MB2"<<endl;
 }
-
+void B::MB3(){
+	cout<<"MB3"<<endl;
+}
 int main(){
 	B b1;
 	b1.MB1();
 	b1.MB2();
+	b1.MB3();
 	return 0;
 }
